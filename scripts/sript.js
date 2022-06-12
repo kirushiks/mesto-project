7fff96d9de93e230227cce62fe33115aefd943ab
Kirill Korotlov <kirushikss@gmail.com>
2022-06-12 21:55:10 +0300

    js3

--- scripts/sript.js
+++ scripts/sript.js
@@ -9,25 +9,23 @@ const popupCloseAdd = document.querySelector("#popup__newplace_close");
 const popupPhoto = document.querySelector("#popup_photo");
 const popupPhotoClose = document.querySelector("#popup__photo_close");
 
-popupToggleEdit.addEventListener("click", () => {
-  popupEdit.classList.toggle("active");
-});
+const openPopup = (elem) => {
+  elem.classList.add("active");
+}
 
-popupCloseEdit.addEventListener("click", () => {
-  popupEdit.classList.toggle("active");
-});
+const closePopup = (elem) => {
+  elem.classList.remove("active");
+}
 
-popupToggleAdd.addEventListener("click", () => {
-  popupAdd.classList.toggle("active");
-});
+popupToggleEdit.addEventListener("click",  () => openPopup(popupEdit));
 
-popupCloseAdd.addEventListener("click", () => {
-  popupAdd.classList.toggle("active");
-});
+popupCloseEdit.addEventListener("click",  () => closePopup(popupEdit));
 
-popupPhotoClose.addEventListener("click", () => {
-  popupPhoto.classList.toggle("active");
-});
+popupToggleAdd.addEventListener("click",  () => openPopup(popupAdd));
+
+popupCloseAdd.addEventListener("click",  () => closePopup(popupAdd));
+
+popupPhotoClose.addEventListener("click",  () => closePopup(popupPhoto));
 
 const formElement = document.querySelector("#popup_edit .popup__form");
 const profileName = document.querySelector(".profile__title");
@@ -52,15 +50,7 @@ const newplaceForm = document.querySelector("#popup_newplace .popup__form");
 const newplaceNameInput = document.querySelector("#popup_newpPlaceName");
 const newplaceImgInput = document.querySelector("#popup_newpPlaceImg");
 
-const unliked = (e) => {
-  const elementInfo = e.currentTarget.parentElement;
-  elementInfo.removeChild(e.currentTarget);
-  const elementLike = document.createElement("div");
-  elementLike.classList.add("element__like");
-  elementInfo.append(elementLike);
-};
-
-const liking = (e) => {
+const changeLikeStatus = (e) => {
   const elementLike = e.currentTarget;
   if (elementLike.classList.contains("element__like")) {
     elementLike.classList.remove("element__like");
@@ -72,46 +62,27 @@ const liking = (e) => {
 };
 
 const addNewCard = (placeName, picURL) => {
-  const element = document.createElement("div");
-  element.classList.add("element");
 
-  const image = document.createElement("img");
-  image.classList.add("element__image");
-
-  const elementInfo = document.createElement("div");
-  elementInfo.classList.add("element__info");
-
-  const elementTitle = document.createElement("h2");
-  elementTitle.classList.add("element__title");
-
-  const elementLike = document.createElement("div");
-  elementLike.classList.add("element__like");
-
-  image.addEventListener("click", () => {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  const imageElement =  cardElement.querySelector(".element__image");
+  imageElement.addEventListener("click", () => {
     popupPhoto.querySelector(".popup__image").src = picURL;
     popupPhoto.querySelector(".popup__card_title").innerHTML = placeName;
-    popupPhoto.classList.toggle("active");
+    openPopup(popupPhoto)
   });
 
+  imageElement.src = picURL;
 
+  cardElement.querySelector(".element__title").textContent = placeName;
 
-  elementLike.addEventListener("click", liking);
+  cardElement.querySelector(".element__like").addEventListener("click", changeLikeStatus);
 
-  const elementTrashIcon = document.createElement("button");
-  elementTrashIcon.classList.add("element__image_trash");
+  cardElement.querySelector(".element__image_trash").addEventListener("click", (e) => {
+    elements.removeChild(cardElement);
+  });;
 
-  image.src = picURL;
-  elementTitle.textContent = placeName;
-  elementInfo.append(elementTitle, elementLike);
-
-  element.append(elementTrashIcon);
-
-  element.append(image, elementInfo);
-  elements.prepend(element);
-
-  elementTrashIcon.addEventListener("click", (e) => {
-    elements.removeChild(elementTrashIcon.parentElement);
-  });
+  elements.prepend(cardElement);
 };
 
 const newPlaceFormHandler = (evt) => {
@@ -151,6 +122,5 @@ const initialCards = [
 ];
 
 initialCards.forEach((card) => {
-  console.log(card);
   addNewCard(card.name, card.link);
 });
